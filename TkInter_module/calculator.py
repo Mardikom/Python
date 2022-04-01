0dfb8e4f1870a515534c0e307c9779f33122d373
Evgeniy <saliyenko12@gmail.com>
2022-04-01 16:08:32 +0200

    -

--- TkInter_module/calculator.py
+++ TkInter_module/calculator.py
@@ -1,8 +1,9 @@
 import tkinter as t
 from tkinter import ttk
+import re
 
 win = t.Tk() # создаем окно
-win.geometry("425x210+500+50")
+
 win.title("Calculator")
 win.resizable(width=False, height=False)
 
@@ -25,7 +26,7 @@ def digit_button(digit):
     return ttk.Button(text = digit, command = lambda : add_digit(digit))
 
 def add_operation(operation):
-    if entry.get()[-1] in ["-","+","*","/"]:
+    if entry.get()[-1] in "+-*/.":
         entry.delete(len(entry.get())-1, "end")
     entry.insert("end", operation)
 
@@ -37,14 +38,23 @@ def clear():
     entry.delete(0,"end")
     entry.insert("end", 0)
 
+
 def calculate():
     storka = entry.get()
     entry.delete(0, "end")
     entry.insert(0, eval(storka))
 
 def add_dot():
-    entry.insert("end", ".")
-
+    storka = entry.get()
+    if "." not in re.split("[-/*+]", storka)[-1] and storka[-1] not in "+-*/":
+        entry.insert("end", ".")
+        
+def backspace():
+    storka = entry.get()
+    if storka != "0":
+        entry.delete(len(storka)-1, "end")
+    if entry.get() == "" :
+        entry.insert("end", 0)
 
 digit_button(1).grid(row = 4, column = 0)
 digit_button(2).grid(row = 4, column = 1)
@@ -64,9 +74,9 @@ operation_button("*").grid(row = 5, column=3)
 
 
 ttk.Button(text = "=", command=calculate).grid(row = 5, column = 2)
-ttk.Button(text = "C", command=clear).grid(row = 5, column = 1)
-ttk.Button(text = ".", command=add_dot).grid(row = 6, column = 0)
-# coma_button = ttk.Button(text = ",").grid(row = 5, column = 1,padx = 10)
+ttk.Button(text = "C", command=clear).grid(row = 6, column = 0, columnspan=2, sticky="we")
+ttk.Button(text = ".", command=add_dot).grid(row = 5, column = 1)
+ttk.Button(text = "CE", command=backspace).grid(row = 6, column = 2, columnspan=2, sticky="we")
 
 
 win.mainloop()
