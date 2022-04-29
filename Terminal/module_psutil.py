b4f66b835966aab41bbbc918b7ee5f2b9d11fe1f
Evgeniy <saliyenko12@gmail.com>
2022-04-29 19:21:31 +0200

    -

--- Terminal/module_psutil.py
+++ Terminal/module_psutil.py
@@ -1,11 +1,16 @@
+from importlib.resources import path
 import psutil
 
 import psutil as pu
 
-print(pu.cpu_count())
-print(pu.cpu_percent(percpu=True))
-print(pu.virtual_memory())
-print(pu.virtual_memory()[0]/(1024*1024))
-print(pu.sensors_battery())
+# print(pu.cpu_count())
+# print(pu.cpu_percent(percpu=True))
+# print(pu.virtual_memory())
+# print(pu.virtual_memory()[0]/(1024*1024))
+# print(pu.sensors_battery())
 # ДЗ: разобрать еще как минимум 4 любые функции модуля
 
+print("\033[1;32;40mSwap memory:", pu.swap_memory()[0]/(1024*1024))# показывает сколько памяти в свапе
+print("Users:", pu.users()[0][0])# показывает пользователей
+print("Drive format: ", pu.disk_partitions()[0][2])# показывает инфу про накопитель
+print("SSD speed: ",  pu.disk_io_counters()[4]/1024) # показывает скорость скорость и количество записаных/прочитаных мб
