280062c6ff59635de1af8ae112891deafe2a6814
Evgeniy <saliyenko12@gmail.com>
2022-05-06 00:35:40 +0200

    add something to ui

--- TkInter_module/cpu_util.py
+++ TkInter_module/cpu_util.py
@@ -1,17 +1,26 @@
+from calendar import c
+from ipaddress import collapse_addresses
 import tkinter as t
 from tkinter import ttk
 import psutil as pu
 
 win = t.Tk()
+win.geometry("500x500")
 
 progressbars = []
+
+cpuInfoLabel = ttk.Label(text="CPU INFO:")
+cpuInfoLabel.grid(row=0, column=0)
+
+cpuTotalLabel = ttk.Label(text="TOTAL:")
+cpuTotalLabel.grid(row=0, column=1, )
 
 def bars_interface():
     for x in range(8):
         progressbars.append(ttk.Progressbar(value = 45, length=200))
 
     for index, bar in enumerate(progressbars):
-        bar.grid(row = index, column = 0)
+        bar.grid(row = index+1, column = 0)
 
 def bars_config_cpu():
     cores = pu.cpu_percent(percpu=True)
